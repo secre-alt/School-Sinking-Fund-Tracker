34f49820181e8dd3431130f17fb6f03b1ccfde76
secre-alt <triannesano@gmail.com>
2025-09-26 22:41:50 +0800

    up

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,10 @@
 <template>
    <header class="bg-indigo-600 text-white shadow-lg">
-        <div class="container mx-auto px-4 py-6">
+        <div class="container mx-auto px-4 py-3">
             <div class="flex justify-between items-center">
                 <div>
                     <h1 class="text-[20px] font-bold">School Sinking Fund Tracker</h1>
-                    <p class="text-indigo-100">Track student Contributions</p>
+                    <p class="text-indigo-200">Track student Contributions</p>
                 </div>
             </div>
         </div>
