5ec2c560f7c7b64d9eaf50a059740cae4e339861
secre-alt <triannesano@gmail.com>
2025-09-29 00:34:24 +0800

    up

--- src/components/AddEditContributionModal.vue
+++ src/components/AddEditContributionModal.vue
@@ -46,7 +46,7 @@
                             <button type="submit" class="px-4 py-2 bg-indigo-600 text-white rounded-lg hover:bg-indigo-700">
                                 {{ editingContribution ? 'Update' : 'Save' }}
                             </button>
-                            <button type="button" @click="closeModal" class="px-4 py-2 border-2 border-solid border-indigo-500 text-indigo-700 bg-transparent rounded-lg hover:bg-indigo-50">
+                            <button type="button" @click="closeModal" class="px-4 py-2 border-2 border-solid border-indigo-600 text-indigo-600 bg-transparent rounded-lg hover:bg-indigo-50">
                                 Cancel
                             </button>
                         </div>
